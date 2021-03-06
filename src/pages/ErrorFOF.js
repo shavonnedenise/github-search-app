964fcad20098da3f61b9c53e6d61370dd966a6be
Shavonne Cobb <shavonnecobb@Shavonnes-MBP.attlocal.net>
2021-03-06 14:40:27 -0600

    Redesigned simple 404 page

--- src/pages/ErrorFOF.js
+++ src/pages/ErrorFOF.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const ErrorFOF = () => {
      return (
-          <section>
-               <div>404 Page</div>
-               <Link to="/">Return Home</Link>
-          </section>
+          <>
+               <Grid className="error-page-container">
+                    <Button>
+                         <Link to={`/`}>Go back to search repositories</Link>
+                    </Button>
+                    <Paper elevation={3} square className="details-page-card">
+                         <Typography align="center">
+                              <h1 className="error-card-text">
+                                   Error 404
+                              </h1>
+                              <h4 className="error-card-text">
+                                   Uh oh friend - you've reached a page that doesn't exist.
+                              </h4>
+                              <h4 className="error-card-text">
+                                   How about we head back to the <Link to={`/`}>search page</Link> and search for some kitty cats!
+                              </h4>
+                              <p>
+                                   Meow &hearts;
+                              </p>
+                         </Typography>
+                    </Paper>
+               </Grid>
+          </>
      );
 };
 
